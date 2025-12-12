2907b3f0e91e7c933d26086fa22f71aeb64ecdf3
Ashesh Vidyut <asheshvidyut@google.com>
2025-12-12 06:29:37 +0000

    fix icon test in resource link

--- tests/client/test_grpc_transport_session_e2e.py
+++ tests/client/test_grpc_transport_session_e2e.py
@@ -509,6 +509,7 @@ async def test_list_tools_grpc_transport_failure(server_port: int) -> None:
                 "size": None,
                 "annotations": None,
                 "_meta": None,
+                "icon": None,
                 "type": "resource_link",
             },
         ),
